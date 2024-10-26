993bc63d86f1f0dd2b2d0568cbc44ed655ea9514
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-10-25 23:24:46 -0500

    Model 3D jsx

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,13 +1,22 @@
 import './home.css';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
 import Header from '../../layouts/header/header';
+import ModelBalon3d from '../../utils/model3D/Balon3d';
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='h-full'>
+      <Header />
+      <Canvas>
+        <ambientLight />
+        <OrbitControls enableZoom={false} autoRotate={true} enableRotate={false} autoRotateSpeed={1.5} />
+        <Suspense fallback={null}>
+          <ModelBalon3d position={[0, -2, 0]} scale={2.0} />
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
     </div>
   )
 }
