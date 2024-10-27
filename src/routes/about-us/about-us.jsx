7b981aa284b6e0d3e4c4b1ea7c4a16736152ba55
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-10-27 16:55:36 -0500

    Change model3d

--- src/routes/about-us/about-us.jsx
+++ src/routes/about-us/about-us.jsx
@@ -1,10 +1,21 @@
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+import Header from '../../layouts/header/header';
+import ModelBalon3d from '../../utils/model3D/Balon3d';
+
 export default function AboutUs() {
   return (
-    <>
-      <div>about-us</div>
-      <div>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse labore harum ipsum quo eos, animi sed culpa odit non eum aliquam, perspiciatis dignissimos incidunt rerum quaerat est inventore officia maxime.
-      </div>
-    </>
+      <div className='h-full'>
+      <Header />
+      <Canvas>
+        <ambientLight />
+        <OrbitControls enableZoom={false} autoRotate={true} enableRotate={false} autoRotateSpeed={1.5} />
+        <Suspense fallback={null}>
+          <ModelBalon3d position={[0, -2, 0]} scale={2.0} />
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
+    </div>
   )
 }
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,22 +1,9 @@
 import './home.css';
-import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Environment, OrbitControls } from '@react-three/drei';
-import Header from '../../layouts/header/header';
-import ModelBalon3d from '../../utils/model3D/Balon3d';
 
 export default function Home() {
   return (
-    <div className='h-full'>
-      <Header />
-      <Canvas>
-        <ambientLight />
-        <OrbitControls enableZoom={false} autoRotate={true} enableRotate={false} autoRotateSpeed={1.5} />
-        <Suspense fallback={null}>
-          <ModelBalon3d position={[0, -2, 0]} scale={2.0} />
-        </Suspense>
-        <Environment preset="sunset" />
-      </Canvas>
-    </div>
+    <>
+      <h1>Home</h1>
+    </>
   )
 }
