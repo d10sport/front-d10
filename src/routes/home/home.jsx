6daabca2b373113b213946db5f0cb8008e4f920a
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-10-27 18:04:38 -0500

    Resolve error routing

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,8 +1,10 @@
+import Header from '../../layouts/header/header';
 import './home.css';
 
 export default function Home() {
   return (
     <>
+      <Header />
       <h1>Home</h1>
     </>
   )
--- src/routes/router/root.jsx
+++ src/routes/router/root.jsx
@@ -1,35 +1,18 @@
-import { createBrowserRouter } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Services from "../services/services.jsx"
 import AboutUs from '../about-us/about-us.jsx'
 import Contact from '../contact/contact.jsx'
 import ErrorPage from './error.jsx'
 import Home from "../home/home.jsx"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/about-us",
-    element: <AboutUs />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/services",
-    element: <Services />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
-  }
-])
-
-export default router
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  )
+}
