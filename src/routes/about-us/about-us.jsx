1bec1aab02ca18b5df2f42169c824345c75204d6
Deymer Ospina <deymer.ospina@gmail.com>
2024-10-29 00:14:57 -0500

    Design About Us section, redesign and restructure index page and animate navigation.

--- src/routes/about-us/about-us.jsx
+++ src/routes/about-us/about-us.jsx
@@ -3,7 +3,26 @@ import './about-us.css'
 export default function AboutUs() {
   return (
     <div>
-      About us
+      <section className="section">
+      <h1 className="title__section">Conócenos</h1>
+      <h2 className="subtitle__section">Misión:</h2>
+      <p className="text__section text-margin__section">
+        Nuestra misión en D10 SPORT es inspirar y potenciar el rendimiento de atletas y entusiastas del deporte a través de prendas de alta calidad que fusionan diseño innovador, funcionalidad excepcional y estilo moderno. Nos comprometemos a ser la elección preferida para aquellos que buscan no solo ropa deportiva, sino una experiencia que refleje su pasión por el movimiento, la salud y el bienestar. <br/>
+        Nos esforzamos por impulsar un cambio positivo en la vida de las personas, promoviendo un estilo de vida activo y saludable. Buscamos superar las expectativas de nuestros clientes al ofrecer productos que no solo cumplan con los estándares más altos de rendimiento, durabilidad y comodidad, sino que también reflejen nuestra dedicación a la sostenibilidad y responsabilidad social.
+      </p>
+
+      <h2 className="subtitle__section">Vision:</h2>
+      <p className="text__section text-margin__section">
+        Nuestra misión en D10 SPORT es inspirar y potenciar el rendimiento de atletas y entusiastas del deporte a través de prendas de alta calidad que fusionan diseño innovador, funcionalidad excepcional y estilo moderno. Nos comprometemos a ser la elección preferida para aquellos que buscan no solo ropa deportiva, sino una experiencia que refleje su pasión por el movimiento, la salud y el bienestar. <br/>
+        Nos esforzamos por impulsar un cambio positivo en la vida de las personas, promoviendo un estilo de vida activo y saludable. Buscamos superar las expectativas de nuestros clientes al ofrecer productos que no solo cumplan con los estándares más altos de rendimiento, durabilidad y comodidad, sino que también reflejen nuestra dedicación a la sostenibilidad y responsabilidad social.
+      </p>
+
+      <h2 className="subtitle__section">Objetivos:</h2>
+      <p className="text__section text-margin__section">
+        Nuestra misión en D10 SPORT es inspirar y potenciar el rendimiento de atletas y entusiastas del deporte a través de prendas de alta calidad que fusionan diseño innovador, funcionalidad excepcional y estilo moderno. Nos comprometemos a ser la elección preferida para aquellos que buscan no solo ropa deportiva, sino una experiencia que refleje su pasión por el movimiento, la salud y el bienestar. <br/>
+        Nos esforzamos por impulsar un cambio positivo en la vida de las personas, promoviendo un estilo de vida activo y saludable. Buscamos superar las expectativas de nuestros clientes al ofrecer productos que no solo cumplan con los estándares más altos de rendimiento, durabilidad y comodidad, sino que también reflejen nuestra dedicación a la sostenibilidad y responsabilidad social.
+      </p>
+    </section>
     </div>
   )
 }
