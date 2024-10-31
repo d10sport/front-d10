373beede257d3d00ce7e89f1797d5f8ce34ca2c7
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-10-31 00:46:50 -0500

    Changes background and components home

---
+++ src/utils/icons/icons.jsx
@@ -0,0 +1,47 @@
+import logo from '../../assets/icons/logo.png';
+import icon1 from '../../assets/icons/icon1.png';
+import icon2 from '../../assets/icons/icon2.png';
+import icon3 from '../../assets/icons/icon3.png';
+import icon4 from '../../assets/icons/icon4.png';
+
+
+function LogoHeader() {
+  return (
+    <img src={logo} alt="logo D10" className="logo" />
+  )
+}
+
+function Icon1() {
+  return (
+    <img src={icon1} alt="icon" className="img__footer" />
+  )
+}
+
+function Icon2() {
+  return (
+    <img src={icon2} alt="icon" className="img__footer" />
+  )
+}
+
+
+function Icon3() {
+  return (
+    <img src={icon3} alt="icon" className="img__footer" />
+  )
+}
+
+
+function Icon4() {
+  return (
+    <img src={icon4} alt="icon" className="img__footer" />
+  )
+}
+
+
+export {
+  LogoHeader,
+  Icon1,
+  Icon2,
+  Icon3,
+  Icon4
+}
