dca4e0fe80944895390050ad4ec4cb7f129147ae
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-10-31 23:22:16 -0500

    Button login style

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -8,27 +8,27 @@ export default function Header() {
       <LogoHeader />
       <ul className="list__nav">
         <li className="items__nav">
-          <Link to={'/about-us'} className="a-linear__nav text-lg">
+          <Link to={'/about-us'} className="a-linear__nav text-sm">
             Quienes somos
           </Link>
         </li>
         <li className="items__nav">
-          <Link to={'/services'} className="a-linear__nav text-lg">
+          <Link to={'/services'} className="a-linear__nav text-sm">
             Servicios
           </Link>
         </li>
         <li className="items__nav">
-          <Link to={'/contact'} className="a-linear__nav text-lg">
+          <Link to={'/contact'} className="a-linear__nav text-sm">
             Contactanos
           </Link>
         </li>
         <li className="items__nav">
-          <Link to={'/spline'} className="a-linear__nav text-lg">
+          <Link to={'/spline'} className="a-linear__nav text-sm">
             Spline
           </Link>
         </li>
       </ul>
-      <button className="login__nav text-lg">D10+</button>
+      <button className="login__nav text-sm">D10+</button>
     </nav>
 
   );
