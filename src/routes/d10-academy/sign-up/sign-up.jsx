ad269bbf6cb468e32b56035abefb1b2a7ff5764a
Deymer Ospina <deymer.ospina@gmail.com>
2024-11-01 01:32:22 -0500

    Login and registration section added

--- src/routes/d10-academy/sign-up/sign-up.jsx
+++ src/routes/d10-academy/sign-up/sign-up.jsx
@@ -1,5 +1,32 @@
 export default function SignUp() {
   return (
-    <div>sign-up</div>
+    <>
+      <nav className="panel"></nav>
+      <main className="login">
+        <h1 className="title__login">Iniciar Sesión</h1>
+        <div className="container__login">
+          <form action="" className="form__login">
+            <label className="label__login">Correo</label>
+            <input
+            type="text"
+            placeholder="Username@gmail.com"
+            className="input__login"
+          />
+            <label className="label__login">Contraseña</label>
+            <input type="text" placeholder="Password" className="input__login" />
+            <label className="label__login">Confirmar contraseña</label>
+            <input
+            type="text"
+            placeholder="Confirm Password"
+            className="input__login"
+          />
+            <input type="button" value="Registrarse" className="btn__login" />
+          </form>
+          <p className="text__login">
+          ¿Ya tienes una cuenta? <a href="#" className="link__login">Iniciar sesión</a>
+          </p>
+        </div>
+      </main>
+    </>
   )
 }
