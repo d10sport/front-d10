05fcbc471e32da44802fedfe5ee9d595452e96ae
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-11-01 21:47:04 -0500

    Props in header

--- src/utils/icons/icons.jsx
+++ src/utils/icons/icons.jsx
@@ -6,7 +6,7 @@ import icon4 from '../../assets/icons/icon4.png';
 import { Link } from 'react-router-dom';
 
 
-function LogoHeader({props}) {
+function LogoHeader() {
   return (
     <Link to={'/'} >
       <img src={logo} alt="logo D10" className="logo" />
