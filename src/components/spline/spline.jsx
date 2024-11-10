db730b4d9bfd72cbd03f7a4c47cd089d9096d620
Daniel Muñoz <daniel20025febrero29@gmail.com>
2024-11-09 22:23:57 -0500

    Alias imports and delete model glass

--- src/components/spline/spline.jsx
+++ src/components/spline/spline.jsx
@@ -1,10 +1,5 @@
-import ModelBalonGlass from '../../utils/model3D/BalonGlass.jsx';
-import { Environment, OrbitControls } from '@react-three/drei';
-// import ModelBalon3d from '../../utils/model3D/Balon3d.jsx';
-import logo from '../../assets/img/logo_sin_fondo.png';
+import logo from '@assets/img/logo_sin_fondo.png'
 import { useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
 import './spline.css';
 
 export default function SplineModel() {
@@ -17,18 +12,18 @@ export default function SplineModel() {
 
     setTimeout(() => {
       setMoveTitle(true);
-    }, 1000);
+    }, 2000);
 
     setTimeout(() => {
       document.body.classList.remove('overflow-hidden');
       document.getElementById('nav_header').classList.remove('hidden');
       document.querySelector('.wpp').classList.remove('hidden');
       window.scrollTo(0, 0);
-    }, 2500);
+    }, 2800);
 
     setTimeout(() => {
       setIsFinished(true);
-    }, 2600);
+    }, 3000);
   }, []);
 
   return (
@@ -44,34 +39,6 @@ export default function SplineModel() {
           </div>
         </>
       )}
-
-      {/* Balon Glass Model */}
-      {!isFinished && (
-        <div id='model_glass' className="full-screen active animate-jump-in">
-          <Canvas className="w-full h-[105%;] canvas">
-            <ambientLight />
-            <OrbitControls enableZoom={false} autoRotate={false} enableRotate={false} />
-            <Suspense fallback={null}>
-              <ModelBalonGlass position={[0, 0, -60]} scale={1.0} />
-            </Suspense>
-            <Environment preset="sunset" />
-          </Canvas>
-        </div>
-      )}
-
-      {/* Balon 3D Model */}
-      {/* {!isFinished && (
-        <div className="full-screen active">
-          <Canvas className="w-full h-full">
-            <ambientLight />
-            <OrbitControls enableZoom={false} autoRotate={true} enableRotate={false} autoRotateSpeed={1.5} />
-            <Suspense fallback={null}>
-              <ModelBalon3d position={[0, -2.2, 0]} scale={2.3} />
-            </Suspense>
-            <Environment preset="sunset" />
-          </Canvas>
-        </div>
-      )} */}
     </section>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/routes'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@utils': path.resolve(__dirname, 'src/utils')
+    },
+  },
 })
