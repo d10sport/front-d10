a14afb6839e1278e3b238ab4d77a913aa1bc0e8e
Deymer Ospina <deymer.ospina@gmail.com>
2024-12-04 15:19:02 -0500

    Comments on deleted data

--- src/routes/about-us/about-us.jsx
+++ src/routes/about-us/about-us.jsx
@@ -80,11 +80,6 @@ export default function AboutUs() {
             {sectionOne.title}
           </h1>
           <p className="text__aboutus padding-cntr-txt__space text-2xl text-white">
-            {/* En D10 vivimos y respiramos fútbol. Somos una organización
-            comprometida con el desarrollo de talentos en el fútbol, desde los
-            primeros pasos hasta alcanzar su máximo potencial. Donde puedan
-            forjar no solo sus habilidades en el campo, sino también su carácter
-            y amor por el deporte. */}
             {sectionOne.description}
           </p>
         </div>
@@ -100,13 +95,6 @@ export default function AboutUs() {
               {sectionTwo.subtitle}
             </h3>
             <p className="text__aboutus text-white text-2xl">
-              {/* David Urrego, fundador de D10, es un joven de 32 años con una
-              pasión por el fútbol y el desarrollo juvenil. En su experiencia
-              como Futbolista y hoy Orientador de jugadores, ha guiado chicos
-              talentosos en su camino hasta llegar al profesionalismo, creando
-              oportunidades y construyendo sueños. Su visión y liderazgo son
-              fundamentales para el éxito de D10 y la inspiración de la próxima
-              generación de futbolistas */}
               {sectionTwo.description}
             </p>
           </div>
@@ -117,16 +105,6 @@ export default function AboutUs() {
             {sectionThree.title}
           </h3>
           <p className="text__aboutus padding-cntr-txt__space text-white text-2xl">
-            {/* Nuestro principal objetivo es entrar fuerte en el mercado deportivo
-            vistiendo como mínimo un equipo en cada ciudad de Colombia,
-            inicialmente tenemos como primera meta realizar 10 convenios los
-            cuales se harán como pre-venta del proyecto en el segundo semestre
-            del año actual y que nos servirán como modelo de trabajo para que en
-            todo 2025 cumplamos nuestro objetivo principal que es ver D10 en
-            todo el país, teniendo una escala de crecimiento exponencial, donde
-            podamos sumar más servicios a nuestro portafolio como campamentos,
-            experiencias deportivas y como producto final un D10 Cup exclusivo
-            para nuestros clubes asociados. */}
             {sectionThree.description}
           </p>
         </div>
@@ -138,20 +116,6 @@ export default function AboutUs() {
               {sectionFour.title}
             </h3>
             <p className="text__aboutus text-white text-2xl">
-              {/* Nuestra misión en D10 es inspirar y potenciar el rendimiento de
-              atletas y entusiastas del deporte a través de prendas de alta
-              calidad que fusionan diseño innovador, funcionalidad excepcional y
-              estilo moderno. Nos comprometemos a ser la elección preferida para
-              aquellos que buscan no solo ropa deportiva, sino una experiencia
-              que refleje su pasión por el movimiento, la salud y el bienestar.
-              <br />
-              Nos esforzamos por impulsar un cambio positivo en la vida de las
-              personas, promoviendo un estilo de vida activo y saludable.
-              Buscamos superar las expectativas de nuestros clientes al ofrecer
-              productos que no solo cumplan con los estándares más altos de
-              rendimiento, durabilidad y comodidad, sino que también reflejen
-              nuestra dedicación a la sostenibilidad y responsabilidad social. */}
-
               {sectionFour.description}
             </p>
           </div>
@@ -173,25 +137,6 @@ export default function AboutUs() {
               {sectionSix.title}
             </h3>
             <p className="text__aboutus text-white text-2xl">
-              {/* D10 tiene como visión, por medio de una marca de ropa deportiva,
-              tener un proyecto social auto sostenible que llegará a todos los
-              rincones de Colombia que se respire fútbol, donde todas las
-              escuelas que se quieran vincular puedan hacerlo con facilidad,
-              logrando así , tener la mayor cantidad de niños y niñas vistiendo
-              nuestra marca pero también recibiendo un entrenamiento optimo,
-              guiados por sus entrenadores ya formados en nuestra plataforma
-              educativa totalmente gratis. Sumándole por consecuencia a ese buen
-              trabajo aumentar las estadísticas de éxito en los procesos de
-              nuestros deportistas afiliados.
-              <br />
-              En D10 , visualizamos un futuro donde nuestra marca se consolide
-              como un referente en la industria de la ropa deportiva, reconocida
-              por su innovación, calidad superior y compromiso inquebrantable
-              con la excelencia. Aspiramos a ser más que una marca; buscamos
-              convertirnos en un estilo de vida, inspirando a personas de todo
-              el mundo a abrazar la actividad física y alcanzar sus metas a
-              través de prendas deportivas de vanguardia. */}
-
               {sectionSix.description}
             </p>
           </div>
