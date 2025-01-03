1ef66f685b1d696665e819f73164f0e8cea3e263
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-01-03 09:04:01 -0500

    Change msg maintance

--- src/layouts/maintenance/maintenance.jsx
+++ src/layouts/maintenance/maintenance.jsx
@@ -14,23 +14,19 @@ export default function Maintenance({ maintenance, photo }) {
     }
   }, [photo])
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     document.body.classList.add('overflow-hidden');
-  //     document.getElementById('nav_header')?.classList.add('hidden');
-  //     window.scrollTo(0, 0);
-  //   }, 3600);
-  // }, [])
-
   return (
     <div id='section_maintenance' className={`h-screen w-full select-none z-[60] ${maintenance && photo != "" ? 'relative' : 'hidden'}`}>
       <div className='w-full h-full absolute top-0 left-0 bottom-0'>
-        <img className='h-full w-full' src={bgPhoto != "" ? bgPhoto : logo} alt="logo D10" />
+        {bgPhoto != "" && <img className='h-full w-auto' src={bgPhoto} alt="bgPhoto" />}
       </div>
-      <div className='w-full h-full absolute top-0 left-0 bottom-0 bg-black bg-opacity-70'>
-        <div className='w-full h-full flex flex-col justify-center items-center'>
-          <h1 className='text-white text-6xl'>Estamos en mantenimiento</h1>
-          <p className='text-white text-2xl'>Estamos trabajando para mejorar tu experiencia</p>
+      <div className='w-full sm:h-full h-1/5 md:h-full absolute top-0 left-0 bottom-0 bg-transparent user-select-none'>
+        <img className='h-full w-auto' src={logo} alt="logo D10" />
+      </div>
+      <div className='w-full h-full absolute top-0 left-0 bottom-0 bg-transparent user-select-none'>
+        <div className='w-full h-full flex flex-col justify-center gap-4 items-center text-center'>
+          <h1 className='text-white text-9xl font-bold'>D10 +</h1>
+          <h1 className='text-white text-6xl'>¡¡ Llegaremos pronto !!</h1>
+          <p className='text_300 text-2xl'>Estamos trabajando para mejorar tu experiencia.</p>
         </div>
       </div>
     </div>
