2b723bf3d66d273e2d8e25a848e67be0bdb421b8
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-01-10 19:57:51 -0500

    Fix error 500

--- src/layouts/maintenance/maintenance.jsx
+++ src/layouts/maintenance/maintenance.jsx
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types'
 import './maintenance.css';
 
 export default function Maintenance({ maintenance }) {
-  debugger
   Maintenance.propTypes = {
     maintenance: PropTypes.shape({
       active: PropTypes.bool,
@@ -19,7 +18,8 @@ export default function Maintenance({ maintenance }) {
   const [data, setData] = useState(maintenance)
 
   useEffect(() => {
-    if (maintenance.description != "" && maintenance.subtitle != "" && maintenance.title != "") {
+    if(maintenance == undefined) return
+    if (maintenance?.description != "" && maintenance?.subtitle != "" && maintenance?.title != "") {
       if (maintenance.active) {
         document.title = "Mantenimiento"
         document.body.style.overflow = 'hidden';
@@ -32,27 +32,31 @@ export default function Maintenance({ maintenance }) {
   }, [maintenance])
 
   return (
-    <div id='section_maintenance' className={`h-screen w-full select-none z-[60] ${maintenance.active ? 'relative' : 'hidden'}`}>
-      <div className='w-full h-full absolute top-0 left-0 bottom-0'>
-        {data.bg_photo != "" ?
-          (
-            <img className='h-full w-full' src={data.bg_photo} alt="bgPhoto" />
-          ) : (
-            <img className='h-full w-full' src={imageFondo} alt="bgPhoto" />
-          )}
-      </div>
-      <div className='w-full h-full absolute top-0 left-0 bottom-0 bg-transparent user-select-none'>
-        <div className='w-full h-full sm:h-full relative flex flex-col justify-center gap-4 items-center text-center'>
-          {data.bg_photo != "" && (
-            <div className='absolute top-2 left-2 bg-transparent user-select-none'>
-              <img className='h-20 sm:h-auto md:h-auto lg:h-auto w-auto' src={logo} alt="logo D10" />
+    <>
+      {maintenance !== undefined && (
+        <div id='section_maintenance' className={`h-screen w-full select-none z-[60] ${maintenance.active ? 'relative' : 'hidden'}`}>
+          <div className='w-full h-full absolute top-0 left-0 bottom-0'>
+            {data.bg_photo != "" ?
+              (
+                <img className='h-full w-full' src={data.bg_photo} alt="bgPhoto" />
+              ) : (
+                <img className='h-full w-full' src={imageFondo} alt="bgPhoto" />
+              )}
+          </div>
+          <div className='w-full h-full absolute top-0 left-0 bottom-0 bg-transparent user-select-none'>
+            <div className='w-full h-full sm:h-full relative flex flex-col justify-center gap-4 items-center text-center'>
+              {data.bg_photo != "" && (
+                <div className='absolute top-2 left-2 bg-transparent user-select-none'>
+                  <img className='h-20 sm:h-auto md:h-auto lg:h-auto w-auto' src={logo} alt="logo D10" />
+                </div>
+              )}
+              <h1 className='text-white text-9xl font-bold'>{data.title}</h1>
+              <h1 className='text-white text-6xl'>{data.subtitle}</h1>
+              <p className='text_300 text-2xl'>{data.description}</p>
             </div>
-          )}
-          <h1 className='text-white text-9xl font-bold'>{data.title}</h1>
-          <h1 className='text-white text-6xl'>{data.subtitle}</h1>
-          <p className='text_300 text-2xl'>{data.description}</p>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
