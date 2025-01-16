bd31266ea17b1bad0a8ff272a35d3d14504a3b46
Deymer Ospina <deymer.ospina@gmail.com>
2025-01-16 02:42:50 -0500

    Redesign of the services section completed

--- src/routes/services/services.jsx
+++ src/routes/services/services.jsx
@@ -1,26 +1,19 @@
-import fondoHomeD10Academy from '../../assets/img/fondo_home_d10_academy.png';
-import HeaderPage from '../../layouts/header-pages/header-page'
-import SplineModel from '@components/spline/spline.jsx';
-import { useEffect, useState, useContext } from 'react';
-import { ImageLogo } from '@utils/imgs/imgs.jsx'
-import AppContext from '@context/app-context';
-import axios from 'axios';
-import './services.css'
+import HeaderPage from "../../layouts/header-pages/header-page";
+import SplineModel from "@components/spline/spline.jsx";
+import { ImageLogo } from "@utils/imgs/imgs.jsx";
+import AppContext from "@context/app-context";
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import "./services.css";
 
 export default function Services() {
   const context = useContext(AppContext);
   const urlApi = context.urlApi;
   const apiKey = context.apiKey;
 
-  const [dataHeader, setDataHeader] = useState({
-    logo: '',
-    bg_photo: '',
-    navStyle: {}
-  });
-
-  const [sectionOne, setSectionOne] = useState({
-    title: "",
-  });
+  // const [sectionOne, setSectionOne] = useState({
+  //   title: "",
+  // });
 
   const [sectionTwo, setSectionTwo] = useState({
     photo: "",
@@ -52,26 +45,10 @@ export default function Services() {
         },
       })
       .then((response) => {
-        setSectionOne(response.data[0].section_one);
+        // setSectionOne(response.data[0].section_one);
         setSectionTwo(response.data[0].section_two);
         setSectionThree(response.data[0].section_three);
         setSectioFour(response.data[0].section_four);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  function getDateLayout() {
-    axios.get(`${urlApi}landing/g/layout`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'api-key': apiKey
-      }
-    })
-      .then((response) => {
-        if (response.data?.length == 0 || response.data[0] == undefined) return;
-        setDataHeader(response.data[0].header);
       })
       .catch((error) => {
         console.error(error);
@@ -89,127 +66,62 @@ export default function Services() {
 
       <SplineModel />
 
-      <div className="h-20 text-center mt-12">
-        <h1 className="text_300 text-4xl font-extrabold">{sectionOne.title}</h1>
-      </div>
-      <div className="w-full grid items-center gap-52 mb-12 py-10">
-        <section id="first_services" className="w-full h-96">
-          <div className="flex justify-between w-full h-full">
-            <aside className="relative w-1/2 bg-transparent h-full">
-              <div className="absolute left-36 top-0 z-10 grid gap-4">
-                <span className="text-xl text_300">
-                  {sectionTwo.title}
-                </span>
-                <h2 className="text-white text-3xl">
-                  {sectionTwo.subtitle}
-                </h2>
-              </div>
-              <div className="absolute top-24 -right-36 z-10 rounded-3xl w-full h-64">
-                <aside className="relative h-full w-full">
-                  <div className="absolute top-0 left-0 flex p-4 justify-center flex-col h-full items-center">
-                    <span className="relative text-pretty text-xl inset-0.5 text-white font-medium rounded-xl w-full z-40">
-                      {sectionTwo.description}
-                    </span>
-                  </div>
-                  <img
-                    className="aspect-video absolute left-0 top-0 h-full w-full opacity-20 rounded-3xl z-10 object-cover"
-                    src={fondoHomeD10Academy}
-                  />
-                  <div className="absolute h-full w-full bg-gray-200 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10"></div>
-                  <div className="absolute bg-white/25 -right-1/2 blur-[45px] h-full w-full rounded-3xl z-20"></div>
-                </aside>
-              </div>
-            </aside>
-            <aside className="relative w-1/2 h-full right-30">
-              <div className="absolute">
-                {sectionTwo.photo != "" ? (
-                  <img src={sectionTwo.photo} alt="img" className="rounded-3xl object-cover" />
-                ) : (
-                  <ImageLogo className="rounded-3xl object-cover" alt="img" />
-                )}
-              </div>
-            </aside>
-          </div>
-        </section>
+      {/* <div className="col-title__services">
+        <h1 className="one-title__services">Services</h1>
+      </div> */}
 
-        <section id="second_services" className="w-full h-96">
-          <div className="flex justify-between w-full h-full">
-            <aside className="relative w-1/2 h-full">
-              <div className="absolute right-0">
-                {sectionThree.photo != "" ? (
-                  <img src={sectionThree.photo} alt="img" className="rounded-3xl object-cover" />
-                ) : (
-                  <ImageLogo className="rounded-3xl object-cover" alt="img" />
-                )}
-              </div>
-            </aside>
-            <aside className="relative h-full w-1/2 bg-transparent">
-              <div className="absolute right-36 top-0 z-10 grid gap-4">
-                <span className="text-xl text_300 text-end">
-                  {sectionThree.title}
-                </span>
-                <h2 className="text-white text-3xl">
-                  {sectionThree.subtitle}
-                </h2>
-              </div>
-              <div className="absolute top-24 -left-36 left z-10 rounded-3xl w-full h-64">
-                <aside className="relative h-full w-full">
-                  <div className="absolute top-0 left-0 flex p-4 justify-center flex-col h-full items-center">
-                    <span className="relative text-pretty text-xl inset-0.5 text-white font-medium rounded-xl w-full z-40">
-                      {sectionThree.description}
-                    </span>
-                  </div>
-                  <img
-                    className="aspect-video absolute left-0 top-0 h-full w-full opacity-20 rounded-3xl z-10 object-cover"
-                    src={fondoHomeD10Academy}
-                  />
-                  <div className="absolute h-full w-full bg-gray-200 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10"></div>
-                  <div className="absolute bg-white/25 -left-1/2 blur-[45px] h-full w-full rounded-3xl z-20"></div>
-                </aside>
-              </div>
-            </aside>
+      <section className="services">
+        <div className="col-text__services">
+          <div className="cntr-text__services">
+            <h1 className="title__services"> {sectionTwo.title} </h1>
+            <h2 className="subtitle__services"> {sectionTwo.subtitle} </h2>
+            <p className="text__services"> {sectionTwo.description} </p>
           </div>
-        </section>
+          <div className="cntr-text__services cntr-text--bg">
+            <h1 className="title__services"> {sectionThree.title} </h1>
+            <h2 className="subtitle__services">{sectionThree.subtitle}</h2>
+            <p className="text__services">{sectionThree.description}</p>
+          </div>
+          <div className="cntr-text__services">
+            <h1 className="title__services"> {sectionFour.title} </h1>
+            <h2 className="subtitle__services"> {sectionFour.subtitle} </h2>
+            <p className="text__services">{sectionFour.description}</p>
+          </div>
+        </div>
+        <div className="col-img__services">
+          <div className="cntr-img__services">
+            {sectionTwo.photo != "" ? (
+              <img src={sectionTwo.photo} alt="img" className="img__services" />
+            ) : (
+              <ImageLogo className="rounded-3xl object-cover" alt="img" />
+            )}
+          </div>
 
-        <section id="third_services" className="w-full h-96 mb-14">
-          <div className="flex justify-between w-full h-full px-8">
-            <aside className="relative h-full w-1/2 bg-transparent">
-              <div className="absolute left-20 top-0 z-10 grid gap-4">
-                <span className="text-xl text_300">
-                  {sectionFour.title}
-                </span>
-                <h2 className="text-white text-3xl">
-                  {sectionFour.subtitle}
-                </h2>
-              </div>
-              <div className="absolute top-24 -right-20 z-10 rounded-3xl w-full h-64">
-                <aside className="relative h-full w-full">
-                  <div className="absolute top-0 left-0 flex p-4 justify-center flex-col h-full items-center">
-                    <span className="relative text-pretty text-xl inset-0.5 text-white font-medium rounded-xl w-full z-40">
-                      {sectionFour.description}
-                    </span>
-                  </div>
-                  <img
-                    className="aspect-video absolute left-0 top-0 h-full w-full opacity-20 rounded-3xl z-10 object-cover"
-                    src={fondoHomeD10Academy}
-                  />
-                  <div className="absolute h-full w-full bg-gray-200 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10"></div>
-                  <div className="absolute bg-white/15 -right-1/2 blur-[45px] h-full w-full rounded-3xl z-20"></div>
-                </aside>
-              </div>
-            </aside>
-            <aside className="relative w-1/2 h-full">
-              <div className="absolute w-full right-24">
-                {sectionFour.photo != "" ? (
-                  <img src={sectionFour.photo} alt="img" className="rounded-3xl object-cover" />
-                ) : (
-                  <ImageLogo className="rounded-3xl w-full h-full object-cover" alt="img" />
-                )}
-              </div>
-            </aside>
+          <div className="cntr-img__services">
+            {sectionThree.photo != "" ? (
+              <img
+                src={sectionThree.photo}
+                alt="img"
+                className="img__services"
+              />
+            ) : (
+              <ImageLogo className="rounded-3xl object-cover" alt="img" />
+            )}
           </div>
-        </section>
-      </div>
+
+          <div className="cntr-img__services">
+            {sectionFour.photo != "" ? (
+              <img
+                src={sectionFour.photo}
+                alt="img"
+                className="img__services"
+              />
+            ) : (
+              <ImageLogo className="rounded-3xl object-cover" alt="img" />
+            )}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
