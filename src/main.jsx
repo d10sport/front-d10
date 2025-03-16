d18727acbe5a95b368c496f29b3b945681c385f9
Deymer Ospina <deymer.ospina@gmail.com>
2025-03-15 22:29:28 -0500

    Revert "New font applied"
    This reverts commit 4394fd98c993992899acc2cc1e7cd9179d03b6a7.

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,6 @@ import { HashRouter } from "react-router-dom";
 import App from "./app/App";
 import "./index.css";
 
-// fonts
-import "./fonts/Robson-Bold.otf";
-import "./fonts/Robson-Extrabold.otf";
-import "./fonts/Robson-Extralight.otf";
-import "./fonts/Robson-Light.otf";
-import "./fonts/Robson-Medium.otf";
-import "./fonts/Robson-Regular.otf";
-import "./fonts/Robson-Semibold.otf";
-import "./fonts/Robson-Thin.otf";
-
 createRoot(document.getElementById("root")).render(
   <HashRouter>
     <AppProvider>
